ecf1b3f09ca15982f54ebf6c318c11129edd76ed
letsmut <lets2rs@126.com>
2023-12-06 06:57:49 +0000

    update

--- zig/sort.zig
+++ zig/sort.zig
@@ -1,27 +1,41 @@
-///sort
+///sort do myself
 const std = @import("std");
 const assert = std.debug.assert;
-const expect = std.testing.expect;
 const eql = std.mem.eql;
 
-pub fn main() void {
+//test
+const expect = std.testing.expect;
+
+pub fn main() !void {
     var a = [_]i32{ 9, 3, 1, 5, 2, 7, 3, 8 };
-    insertSort(i32, &a, 0, 8);
-    for (a) |x| {
-        std.debug.print("{3d}", .{x});
+    //insertSort(i32, &a, 0, 8);
+    insertSort(i32, a[0..], 0, 8);
+    for (&a) |*x| {
+        std.debug.print("{d}\t", .{x.*});
+        std.debug.print("{*}", .{x});
+        std.debug.print("\n", .{});
     }
-    std.debug.print("\n", .{});
 }
 
-pub fn insertSort(comptime T: type, items: []T, a: usize, b: usize) void {
-    assert(a <= b);
+test "sort case" {
+    var b: usize = 0;
+    std.debug.print("usize(0-1):{d}\n", .{b -% 1});
+    //var e: usize = 8;
+    var i = b + 1;
+    try expect(usize == @TypeOf(i));
+    var a = [_]i32{ 9, 3, 1, 5, 2, 7, 3, 8 };
+    try expect(usize == @TypeOf(a.len));
+    try expect(1 == 1);
+}
 
-    var i = a + 1;
-    //try expect(i32 == @TypeOf(i));
-    while (i < b) : (i += 1) {
+pub fn insertSort(comptime T: type, items: []T, b: usize, e: usize) void {
+    assert(b <= e);
+
+    var i = b + 1;
+    while (i < e) : (i += 1) {
         var j = i;
         const base: T = items[i];
-        while (j > a and items[j - 1] > base) : (j -= 1) {
+        while (j > b and items[j - 1] > base) : (j -= 1) {
             items[j] = items[j - 1];
         }
         items[j] = base;

2fe3876a175f23d2d865ddd81f58d0a0b4c94326
letsmut <lets2rs@126.com>
2023-12-28 08:19:18 +0000

    update zig echo

---
+++ zig/src/echo.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const net = std.net;
+const mode = std.io.evented;
+
+//疑惑:一次nc和ctrl+c之后,echo就不可用(设置非阻塞亦如此)
+pub fn main() !void {
+    const localhost = try net.Address.parseIp("127.0.0.1", 9000);
+    var server = net.StreamServer.init(.{
+        .reuse_address = true,
+    });
+    defer server.deinit();
+
+    try server.listen(localhost);
+    while (true) {
+        var conn = try server.accept();
+        defer conn.stream.close();
+        errdefer conn.stream.close();
+        echo(conn) catch |err| {
+            switch (err) {
+                else => continue,
+            }
+            continue;
+        };
+    }
+}
+
+fn echo(conn: net.StreamServer.Connection) !void {
+    while (true) {
+        var buffer: [8096]u8 = undefined;
+        var n = try conn.stream.reader().read(buffer[0..]);
+        _ = try conn.stream.writer().write(buffer[0..n]);
+    }
+}

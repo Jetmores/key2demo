43c80f84e913c9cb946299bf11d114f846b75cb8
letsmut <lets2rs@126.com>
2024-01-03 06:09:24 +0000

    update cc

--- cc/src/lib.cc
+++ cc/src/lib.cc
@@ -1,5 +1,7 @@
 //os
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
 //lib
 #include <pthread.h>
@@ -8,19 +10,27 @@
 #include <cstdio>
 #include <cstdlib>
 
+#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-void perrorExit(const char* str){
-    perror(str);
-    pthread_exit(NULL);
-    exit(1);
-}
 
-int Listen(const char* ip,short post) {
+int Listen(const char* ip,short port) {
     int fd=-1;
+    int opt=1;
+    struct sockaddr_in addr;
 
     fd=socket(AF_INET,SOCK_STREAM,0);
     if(fd==-1){
-        perrorExit("socker error");
+        handle_error("socket");
     }
-    return 0;
+    setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(port);
+    inet_pton(AF_INET,ip,&addr.sin_addr.s_addr);
+    if(-1==bind(fd,(struct sockaddr*)&addr,sizeof(addr))){
+        handle_error("bind");
+    }
+    if (listen(fd, 128) == -1){
+        handle_error("listen");
+    }
+    return fd;
 }

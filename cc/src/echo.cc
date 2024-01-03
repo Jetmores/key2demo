7dbdb6216c1c94a4345864a4b628b864df07ddf8
letsmut <lets2rs@126.com>
2024-01-03 07:13:45 +0000

    update

--- cc/src/echo.cc
+++ cc/src/echo.cc
@@ -1,8 +1,45 @@
+#include <unistd.h>
+
 #include "lib.cc"
 
 using namespace std;
 
 int main(void){
-    printf("Hello World\n");
+    //printf("Hello World\n");
+    int cfd=-1;
+    struct sockaddr_in caddr;
+    socklen_t clen=0;
+    int n=-1;
+    char buf[8]={0,};
+
+    int lfd=Listen("0.0.0.0",9990);
+
+    while (1)
+    {
+        cfd = accept(lfd, (struct sockaddr *)&caddr, &clen);
+        if (cfd == -1){
+            handle_error("accept");
+        }
+        while (1){
+            n = read(cfd, buf, sizeof(buf));
+            if(n==-1){
+                handle_error("read");
+            }else if(n==0){
+                printf("read zero\n");
+                close(cfd);
+                break;
+            }
+            usleep(1000000);
+            n=write(cfd,buf,n);
+            printf("write :%d\n",n);
+            if(n==-1){
+                handle_error("write");
+            }else if(n==0){
+                printf("write zero\n");
+                close(cfd);
+                break;
+            }
+        }
+    }
     return 0;
 }

81450b53230741f3f5dcb996b26386b37875daec
letsmut <lets2rs@126.com>
2024-01-05 02:28:09 +0000

    update cc for mio

--- cc/src/lib.cc
+++ cc/src/lib.cc
@@ -1,58 +1,62 @@
-//os
-#include <sys/socket.h>
-#include <sys/types.h>
+// os
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-//lib
+// lib
 #include <pthread.h>
 
-//std
+// std
 #include <cstdio>
 #include <cstdlib>
 
-#define handle_error(msg) do { perror(msg); pthread_exit(NULL); } while (0)
+#define handle_error(msg)   \
+    do {                    \
+        perror(msg);        \
+        pthread_exit(NULL); \
+    } while (0)
 
-
-void doNothing(int signo){
-    printf("catch number:%d\n",signo);
+void doNothing(int signo)
+{
+    printf("catch number:%d\n", signo);
     return;
 }
 
-
-int Listen(const char* ip,short port) {
-    int fd=-1;
-    int opt=1;
+int Listen(const char* ip, short port)
+{
+    int fd = -1;
+    int opt = 1;
     struct sockaddr_in addr;
 
-    fd=socket(AF_INET,SOCK_STREAM,0);
-    if(fd==-1){
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
         handle_error("socket");
     }
-    setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
-    addr.sin_family=AF_INET;
-    addr.sin_port=htons(port);
-    inet_pton(AF_INET,ip,&addr.sin_addr.s_addr);
-    if(-1==bind(fd,(struct sockaddr*)&addr,sizeof(addr))){
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &addr.sin_addr.s_addr);
+    if (-1 == bind(fd, (struct sockaddr*)&addr, sizeof(addr))) {
         handle_error("bind");
     }
-    if (listen(fd, 128) == -1){
+    if (listen(fd, 128) == -1) {
         handle_error("listen");
     }
     return fd;
 }
 
-
-void set_fl(int fd, int flags){
+void set_fl(int fd, int flags)
+{
     int val;
 
-    if ((val = fcntl(fd, F_GETFL, 0)) < 0){
+    if ((val = fcntl(fd, F_GETFL, 0)) < 0) {
         handle_error("fcntl F_GETFL error");
     }
 
     val |= flags;
 
-    if (fcntl(fd, F_SETFL, val) < 0){
+    if (fcntl(fd, F_SETFL, val) < 0) {
         handle_error("fcntl F_SETFL error");
     }
 }
--- cc/src/mio.cc
+++ cc/src/mio.cc
@@ -1,14 +1,12 @@
-//前提1:正常关闭client
-//结果1:epollin,read=0
-//前提2:服务端还在写时关闭client
-//结果2:epollin,epollerr,epollhup,write=-1 errno=EPIPE
+// 前提1:正常关闭client
+// 结果1:epollin,read=0
+// 前提2:服务端还在写时关闭client
+// 结果2:epollin,epollerr,epollhup,write=-1 errno=EPIPE
 
-
-
-#include <unistd.h>
 #include <errno.h>
 #include <signal.h>
 #include <sys/epoll.h>
+#include <unistd.h>
 
 #include "lib.cc"
 
@@ -17,52 +15,54 @@ using namespace std;
 void doUseFd(int fd);
 void pEventBit(uint32_t events);
 
-int efd=-1;
-int main(void){
+int efd = -1;
+int main(void)
+{
     struct sockaddr_in addr;
     socklen_t addrlen;
-    int n=-1,cfd=-1;
-    struct epoll_event ev,events[3];
+    int n = -1, cfd = -1;
+    struct epoll_event ev, events[3];
     struct sigaction action;
-    action.sa_handler=doNothing;
-    //action.sa_flags =SA_INTERRUPT;
-    //默认重启被打断的慢速read调用,但可手动设置=SA_INTERRUPT从而不重启
-    sigaction(SIGUSR1,&action,NULL);
-    sigaction(SIGPIPE,&action,NULL);
+    action.sa_handler = doNothing;
+    // action.sa_flags =SA_INTERRUPT;
+    // 默认重启被打断的慢速read调用,但可手动设置=SA_INTERRUPT从而不重启
+    sigaction(SIGUSR1, &action, NULL);
+    sigaction(SIGPIPE, &action, NULL);
 
-    int lfd=Listen("0.0.0.0",9990);
-    efd=epoll_create(1);
-    if(efd==-1){
+    int lfd = Listen("0.0.0.0", 9990);
+    efd = epoll_create(1);
+    if (efd == -1) {
         handle_error("epoll_create");
     }
-    ev.events=EPOLLIN;
-    ev.data.fd=lfd;
-    if(-1==epoll_ctl(efd,EPOLL_CTL_ADD,lfd,&ev)){
+    ev.events = EPOLLIN;
+    ev.data.fd = lfd;
+    if (-1 == epoll_ctl(efd, EPOLL_CTL_ADD, lfd, &ev)) {
         handle_error("epoll_ctl");
     }
-    for(;;){
-        n=epoll_wait(efd,events,3,-1);
-        if(n==-1){
+    for (;;) {
+        n = epoll_wait(efd, events, 3, -1);
+        if (n == -1) {
             handle_error("epoll_wait");
         }
-        for(int i=0;i<n;++i){
+        for (int i = 0; i < n; ++i) {
             pEventBit(events[i].events);
-            if(events[i].data.fd==lfd){
-                cfd=accept(lfd,(struct sockaddr*)&addr,&addrlen);
-                if(cfd==-1){
+            if (events[i].data.fd == lfd) {
+                cfd = accept(lfd, (struct sockaddr*)&addr, &addrlen);
+                if (cfd == -1) {
                     handle_error("accept");
                 }
-                printf("accept cfd:%d\n",cfd);
-                set_fl(cfd,O_NONBLOCK);ev.events=EPOLLIN|EPOLLET;
-                //ev.events=EPOLLIN;
-                //ev.events=EPOLLIN|EPOLLONESHOT;//only once,need register again
-                ev.data.fd=cfd;
-                if(-1==epoll_ctl(efd,EPOLL_CTL_ADD,cfd,&ev)){
+                printf("accept cfd:%d\n", cfd);
+                set_fl(cfd, O_NONBLOCK); // match doUseFdEt
+                ev.events = EPOLLIN | EPOLLET; // match doUseFdEt
+                // ev.events=EPOLLIN;
+                // ev.events=EPOLLIN|EPOLLONESHOT;//only once,need register again
+                ev.data.fd = cfd;
+                if (-1 == epoll_ctl(efd, EPOLL_CTL_ADD, cfd, &ev)) {
                     handle_error("epoll_ctl after accept");
                 }
-            }else{
-                printf("events[%d].data.fd:%d\n",i,events[i].data.fd);
-                //doUseFd(events[i].data.fd);
+            } else {
+                printf("events[%d].data.fd:%d\n", i, events[i].data.fd);
+                // doUseFd(events[i].data.fd);
                 doUseFdEt(events[i].data.fd);
             }
         }
@@ -70,28 +70,72 @@ int main(void){
     return 0;
 }
 
-void doUseFdEt(int cfd){
+void doUseFdEt(int cfd)
+{
+    char buf[8] = {
+        0,
+    };
+    int n = -1;
+    for (;;) {
+        n = read(cfd, buf, sizeof(buf));
+        if (n == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) { // no data to read, read reback immediately
+                // printf("read again\n");
+                break;
+            } else {
+                handle_error("read");
+            }
+        } else if (n == 0) {
+            printf("read zero\n");
+            epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
+            close(cfd);
+            return;
+        }
+
+        n = write(cfd, buf, n);
+        printf("write :%d\n", n);
+        if (n == -1) { // do after ignore SIGPIPE
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                printf("write EAGAIN\n");
+                continue;
+            } else if (errno == EPIPE) {
+                epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
+                close(cfd);
+                return;
+            } else {
+                handle_error("write");
+            }
+        } else if (n == 0) {
+            printf("write zero\n");
+            epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
+            close(cfd);
+            return;
+        }
+    }
     return;
 }
 
-void doUseFd(int cfd){
-    char buf[8]={0,};
-    int n=-1;
+void doUseFd(int cfd)
+{
+    char buf[8] = {
+        0,
+    };
+    int n = -1;
 ReadAgain:
     n = read(cfd, buf, sizeof(buf));
-    if (n == -1){
-        if (errno == EAGAIN || errno == EWOULDBLOCK){ // no data to read, read reback immediately
+    if (n == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) { // no data to read, read reback immediately
             // printf("read again\n");
             goto ReadAgain;
-        }else if (errno == EINTR){ // block mode only
+        } else if (errno == EINTR) { // block mode only
             printf("read EINTR\n");
             goto ReadAgain;
-        }else{
+        } else {
             handle_error("read");
         }
-    }else if (n == 0){
+    } else if (n == 0) {
         printf("read zero\n");
-        epoll_ctl(efd,EPOLL_CTL_DEL,cfd,NULL);
+        epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
         close(cfd);
         return;
     }
@@ -99,72 +143,74 @@ ReadAgain:
 WriteAgain:
     n = write(cfd, buf, n);
     printf("write :%d\n", n);
-    if (n == -1){ // do after ignore SIGPIPE
-        if (errno == EAGAIN || errno == EWOULDBLOCK){ // errno==EINTR,not happen when nonblock
+    if (n == -1) { // do after ignore SIGPIPE
+        if (errno == EAGAIN || errno == EWOULDBLOCK) { // errno==EINTR,not happen when nonblock
             goto WriteAgain;
-        }else if (errno == EINTR){ // block mode only
+        } else if (errno == EINTR) { // block mode only
             printf("write EINTR\n");
             goto WriteAgain;
-        }else if (errno == EPIPE){
+        } else if (errno == EPIPE) {
+            epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
             close(cfd);
             return;
-        }else{
+        } else {
             handle_error("write");
         }
-    }else if (n == 0){
+    } else if (n == 0) {
         printf("write zero\n");
-        epoll_ctl(efd,EPOLL_CTL_DEL,cfd,NULL);
+        epoll_ctl(efd, EPOLL_CTL_DEL, cfd, NULL);
         close(cfd);
         return;
     }
     return;
 }
 
-void pEventBit(uint32_t events){
-    printf("events:%#x\n",events);
-    if(events&EPOLLIN){
+void pEventBit(uint32_t events)
+{
+    printf("events:%#x\n", events);
+    if (events & EPOLLIN) {
         printf("EPOLLIN\n");
     }
-    if(events&EPOLLPRI){
+    if (events & EPOLLPRI) {
         printf("EPOLLPRI\n");
     }
-    if(events&EPOLLOUT){
+    if (events & EPOLLOUT) {
         printf("EPOLLOUT\n");
     }
-    if(events&EPOLLERR){
+    if (events & EPOLLERR) {
         printf("EPOLLERR\n");
     }
-    if(events&EPOLLHUP){
+    if (events & EPOLLHUP) {
         printf("EPOLLHUP\n");
     }
-    if(events&EPOLLRDNORM){
+    if (events & EPOLLRDNORM) {
         printf("EPOLLRDNORM\n");
     }
-    if(events&EPOLLRDBAND){
+    if (events & EPOLLRDBAND) {
         printf("EPOLLRDBAND\n");
     }
-    if(events&EPOLLWRNORM){
+    if (events & EPOLLWRNORM) {
         printf("EPOLLWRNORM\n");
     }
-    if(events&EPOLLWRBAND){
+    if (events & EPOLLWRBAND) {
         printf("EPOLLWRBAND\n");
     }
-    if(events&EPOLLMSG){
+    if (events & EPOLLMSG) {
         printf("EPOLLMSG\n");
     }
-    if(events&EPOLLRDHUP){
+    if (events & EPOLLRDHUP) {
         printf("EPOLLRDHUP\n");
     }
-    if(events&EPOLLEXCLUSIVE){
+    if (events & EPOLLEXCLUSIVE) {
         printf("EPOLLEXCLUSIVE\n");
     }
-    if(events&EPOLLWAKEUP){
+    if (events & EPOLLWAKEUP) {
         printf("EPOLLWAKEUP\n");
     }
-    if(events&EPOLLONESHOT){
+    if (events & EPOLLONESHOT) {
         printf("EPOLLONESHOT\n");
     }
-    if(events&EPOLLET){
+    if (events & EPOLLET) {
         printf("EPOLLET\n");
     }
     return;
